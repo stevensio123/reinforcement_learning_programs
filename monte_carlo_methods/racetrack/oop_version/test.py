302b8f1082a0d4070c67b09ccc619bc938f00194
stevensio123 <stevenscw3253@gmail.com>
2026-04-04 16:26:46 +0800

    added behavior policy
    takes in StateSpace object and epsilon, outputs an array with same shape
    as the state space, with the chosen action for each state according to
    the behavior policy

--- monte_carlo_methods/racetrack/oop_version/test.py
+++ monte_carlo_methods/racetrack/oop_version/test.py
@@ -6,22 +6,40 @@ race_track = ["########",
               "#NN#####",
               "#SS#####"]
 
-# TEST: racetrack object
+print("\nTEST: racetrack object")
 racetrack = utils.Racetrack(race_track)
-print(racetrack.racetrack)
-print(racetrack.start_locs())
-print(racetrack.terminal_locs())
-print(racetrack.racetrack[7][3])
+print(f"racetrack list (cartesian):{racetrack.racetrack}")
+print(f"start locations: {racetrack.start_locs()}")
+print(f"terminal locations: {racetrack.terminal_locs()}")
+print(f"racetrack[7][3]: {racetrack.racetrack[7][3]}")
 
 
-# TEST: next_state function
+print("\nTEST: next_state function")
 state = (0,2,0,0)
 # state = (0,7,0,0) # out of bounds
-action = [1,1]
+action = [3,3]
+print(f"Current state: {state}")
+print(f"Action taken: {action}")
 try:
-    x, y , vx, vy = utils.next_state(racetrack.racetrack, state, action)
-    print(x, y, vx, vy)
+    x, y , vx, vy = utils.get_next_state(racetrack.racetrack, state, action)
+    print(f"Next state: ({x}, {y}, {vx}, {vy})")
 except TypeError:
     print("Crashed or out of bounds")
 
 
+print("\nTEST: action space function")
+state = (0,2,0,0)
+print(f"Action space for {state}: {utils.get_action_space(state)}")
+state = (0,2,4,4)
+print(f"Action space for {state}: {utils.get_action_space(state)}")
+
+print("\nTEST: state space class")
+state_space = utils.StateSpace(race_track)
+print(f"State values shape: {state_space.state_values.shape}")
+print(f"State value for (0,2,0,0): {state_space.get_state_value((0,2,0,0))}")
+
+print("\nTEST: behavior policy function")
+policy = utils.behavior_policy(state_space, epsilon=0.2)
+print(f"Policy shape: {policy.shape}")
+print(f"Policy for (0,2,0,0): {policy[0][2][0][0]}")
+print(f"Policy for (0,2,4,4): {policy[0][2][4][4]}")
