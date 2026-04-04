47e0a911bb892bce14e593c4c67489d3b7180bc8
stevensio123 <stevenscw3253@gmail.com>
2026-04-04 18:44:19 +0800

    added back fop and oop incomplete solutions for ch5

---
+++ monte_carlo_methods/racetrack/oop_version/test.py
@@ -0,0 +1,45 @@
+import racetrack_utils as utils
+import numpy as np
+race_track = ["########",
+              "#NNNNNNE",
+              "#NN#####",
+              "#NN#####",
+              "#SS#####"]
+
+print("\nTEST: racetrack object")
+racetrack = utils.Racetrack(race_track)
+print(f"racetrack list (cartesian):{racetrack.racetrack}")
+print(f"start locations: {racetrack.start_locs()}")
+print(f"terminal locations: {racetrack.terminal_locs()}")
+print(f"racetrack[7][3]: {racetrack.racetrack[7][3]}")
+
+
+print("\nTEST: next_state function")
+state = (0,2,0,0)
+# state = (0,7,0,0) # out of bounds
+action = [3,3]
+print(f"Current state: {state}")
+print(f"Action taken: {action}")
+try:
+    x, y , vx, vy = utils.get_next_state(racetrack.racetrack, state, action)
+    print(f"Next state: ({x}, {y}, {vx}, {vy})")
+except TypeError:
+    print("Crashed or out of bounds")
+
+
+print("\nTEST: action space function")
+state = (0,2,0,0)
+print(f"Action space for {state}: {utils.get_action_space(state)}")
+state = (0,2,4,4)
+print(f"Action space for {state}: {utils.get_action_space(state)}")
+
+print("\nTEST: state space class")
+state_space = utils.StateSpace(race_track)
+print(f"State values shape: {state_space.state_values.shape}")
+print(f"State value for (0,2,0,0): {state_space.get_state_value((0,2,0,0))}")
+
+print("\nTEST: behavior policy function")
+policy = utils.behavior_policy(state_space, epsilon=0.2)
+print(f"Policy shape: {policy.shape}")
+print(f"Policy for (0,2,0,0): {policy[0][2][0][0]}")
+print(f"Policy for (0,2,4,4): {policy[0][2][4][4]}")
