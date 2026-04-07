e4a8bb794f5c95392f4e0e97f7f9a58a93f3b41a
stevensio123 <stevenscw3253@gmail.com>
2026-04-07 08:35:43 +0800

    fixed code type in incremental_prediction function

--- monte_carlo_methods/racetrack/oop_version/racetrack.py
+++ monte_carlo_methods/racetrack/oop_version/racetrack.py
@@ -3,35 +3,35 @@ import racetrack_utils as utils
 
 gamma = 0.9
 
-def incremental_prediction(Racetrack, episode, cum_IS):
-  
-  episode.reverse()
-  G = 0
-  W = 1
-  for step in range(len(episode)):
-    G = (gamma*G) - 1
-    y,x,v1,v2 = episode[step][0] # state
-    a1,a2 = episode[step][1] # action
-    cum_IS[y,x,v1,v2,a1,a2] += (cum_IS[y,x,v1,v2,a1,a2] + W)
-    # find q(s,a) of 
-    next_state = utils.get_next_state(Racetrack.racetrack, s, a)
-    state_value = Racetrack.get_state_value(next_state)
-
-
-def policy_control(Racetrack, behaviour_policy):
-  # cumulative sum sampling ratio: state = 4d, action = 3d; total 7d
-  cum_IS = np.zeros((len(Racetrack.racetrack),len(Racetrack.racetrack[0]),5,5,3,3),int)
-  while True:
-    episode_count = 0
-    episode = utils.Episode(Racetrack, behaviour_policy)
-    if episode.generate(Racetrack):
-      episode_count += 1
-      incremental_prediction(episode, cum_IS)
-    else:
-      pass
-
 state = (1, 1, 1, 1)
 a = (-1, 1)
+
+def incremental_prediction(Racetrack, episode, cum_IS, gamma=0.9):
+    
+    episode.reverse()
+    G = 0
+    W = 1
+    for step in range(len(episode)):
+        G = (gamma*G) - 1
+        x,y,v1,v2 = episode[step][0] # state
+        a1,a2 = episode[step][1] # action
+        cum_IS[x,y,v1,v2,a1,a2] += (cum_IS[x,y,v1,v2,a1,a2] + W)
+        # find q(s,a)  
+        next_state = utils.get_next_state(Racetrack.racetrack, (x,y,v1,v2), (a1,a2))
+        next_state_value = Racetrack.get_state_value(next_state)
+
+
+def policy_control(Racetrack, behaviour_policy, gamma=0.9):
+    # cumulative sum sampling ratio: state = 4d, action = 3d; total 7d
+    cum_IS = np.zeros((len(Racetrack.racetrack),len(Racetrack.racetrack[0]),5,5,3,3),int)
+    while True:
+        episode_count = 0
+        episode = utils.Episode(Racetrack, behaviour_policy)
+        if episode.generate(Racetrack):
+            episode_count += 1
+            incremental_prediction(episode, cum_IS)
+        else:
+            pass
 
 
 def main():
@@ -45,7 +45,7 @@ def main():
 
   behavior_policy = utils.behavior_policy(race_track_obj)
 
-  policy_control(race_track_obj, behavior_policy)
-  
+  policy_control(race_track_obj, behavior_policy, gamma)
+    
 
 main()
