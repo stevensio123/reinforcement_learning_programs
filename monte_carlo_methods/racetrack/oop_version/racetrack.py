5011393811695f16d58e83da83641944355928af
stevensio123 <stevenscw3253@gmail.com>
2026-05-07 21:36:41 +0800

    created logging objects for racetrack utils and racetrack
    also created a function to make file handlers with a consistent format.
    should have a log file for each logging levels after logging
    implementations

--- monte_carlo_methods/racetrack/oop_version/racetrack.py
+++ monte_carlo_methods/racetrack/oop_version/racetrack.py
@@ -1,54 +1,73 @@
 import numpy as np
 import racetrack_utils as utils
+import logging
 
-def incremental_prediction(Racetrack, episode, cum_IS, epsilon, gamma=0.9):
+logger = logging.getlogger(__name__)
+logger.setlevel(logging.DEBUG)
 
+# set file handlers for each level
+debug_handler = utils.set_file_handler("debug.log", logging.DEBUG)
+info_handler = utils.set_file_handler("info.log", logging.INFO)
+warn_handler = utils.set_file_handler("warning.log", logging.WARNING)
+error_handler = utils.set_file_handler("error.log", logging.ERROR)
+logger.addhandler(debug_handler)
+logger.addhandler(info_handler)
+logger.addhandler(warn_handler)
+logger.addhandler(error_handler)
+
+
+def incremental_prediction(racetrack, episode, cum_is, epsilon, gamma=0.9):
+    """
+    racetrack : Racetrack class from utils.
+    episode : Episode class from utils, expects episode to be generated.
+
+    """
     episode.reverse()
-    G = 0
-    W = 1
+    g = 0
+    w = 1
     for step in range(len(episode) - 1):
-        # G already accounts for reward -1 for each step
+        # g already accounts for reward -1 for each step
         step += 1
-        G = (gamma * G) - 1
+        g = (gamma * g) - 1
         x, y, v1, v2 = episode[step][0]  # state
         a1, a2 = episode[step][1]  # action
-        cum_IS[x, y, v1, v2, a1, a2] += W
+        cum_is[x, y, v1, v2, a1, a2] += w
 
         print(
-            f"Step T-{step} coordinate: ({x}, {y}, {v1}, {v2}) with action: ({a1}, {a2})"
+            f"step t-{step} coordinate: ({x}, {y}, {v1}, {v2}) with action: ({a1}, {a2})"
         )
 
         # update q(s,a)
-        next_state = utils.get_next_state(Racetrack, episode[step][0], episode[step][1])
-        Racetrack.state_values[x][y][v1][v2] = Racetrack.get_state_value(next_state) + (
-            (W / (cum_IS[x, y, v1, v2, a1, a2]))
-            * (G - Racetrack.get_state_value(next_state))
+        next_state = utils.get_next_state(racetrack, episode[step][0], episode[step][1])
+        racetrack.state_values[x][y][v1][v2] = racetrack.get_state_value(next_state) + (
+            (w / (cum_is[x, y, v1, v2, a1, a2]))
+            * (g - racetrack.get_state_value(next_state))
         )
 
         # update target policy action based on best value of actions
         action_space_ls = utils.get_action_space(
-            episode[step][0], Racetrack.racetrack[x][y]
+            episode[step][0], racetrack.racetrack[x][y]
         )
         # take optimal action according to current state values
         optimal_action_idx = utils.get_optimal_action(
-            Racetrack, episode[step][0], action_space_ls
+            racetrack, episode[step][0], action_space_ls
         )
-        Racetrack.target_policy_dict[x][y][v1][v2] = action_space_ls[optimal_action_idx]
+        racetrack.target_policy_dict[x][y][v1][v2] = action_space_ls[optimal_action_idx]
 
-        print(f"  Action state value: {Racetrack.get_state_value(next_state)}")
+        print(f"  action state value: {racetrack.get_state_value(next_state)}")
         print(
-            f"  Target policy action: {Racetrack.target_policy_dict[x][y][v1][v2]} with state value: {Racetrack.get_state_value(utils.get_next_state(Racetrack, episode[step][0], Racetrack.target_policy_dict[x][y][v1][v2]))}"
+            f"  target policy action: {racetrack.target_policy_dict[x][y][v1][v2]} with state value: {racetrack.get_state_value(utils.get_next_state(racetrack, episode[step][0], racetrack.target_policy_dict[x][y][v1][v2]))}"
         )
 
         # compare if target_policy action matches current action taken
-        if Racetrack.target_policy_dict[x][y][v1][v2] != episode[step][1]:
-            return False
+        if racetrack.target_policy_dict[x][y][v1][v2] != episode[step][1]:
+            return false
 
-        W = W / ((1 - epsilon) + (epsilon / len(action_space_ls)))
+        w = w / ((1 - epsilon) + (epsilon / len(action_space_ls)))
 
 
 def off_policy_control(
-    Racetrack,
+    racetrack,
     epsilon=0.1,
     max_episode_count=10000,
     min_successful_episode=3,
@@ -58,29 +77,29 @@ def off_policy_control(
     """
     max_episode_count: determines the maximum amount of successful episodes needed to be generated.
     min_successful_episode: determines the minimum amount of successful episodes generated for each starting positions.
-    max_episode_generation_attempt: determines the maximum amount of ANY episode generated (success or failure).
+    max_episode_generation_attempt: determines the maximum amount of any episode generated (success or failure).
     """
     # cumulative sum sampling ratio: state = 4d, action = 3d; total 7d
-    cum_IS = np.zeros(
-        (len(Racetrack.racetrack), len(Racetrack.racetrack[0]), 5, 5, 3, 3), int
+    cum_is = np.zeros(
+        (len(racetrack.racetrack), len(racetrack.racetrack[0]), 5, 5, 3, 3), int
     )
-    behaviour_policy = utils.get_policy(Racetrack, epsilon)
+    behaviour_policy = utils.get_policy(racetrack, epsilon)
     episode_count = 0
     ep_generation_attempt = 0
-    successful_ep_counter = [0 for _ in range(len(Racetrack.start_coord_list))]
-    successful_epi_dict = dict(zip(Racetrack.start_coord_list, successful_ep_counter))
-    episode = utils.Episode(Racetrack, behaviour_policy)
-    while True:
+    successful_ep_counter = [0 for _ in range(len(racetrack.start_coord_list))]
+    successful_epi_dict = dict(zip(racetrack.start_coord_list, successful_ep_counter))
+    episode = utils.episode(racetrack, behaviour_policy)
+    while true:
         episode.policy = behaviour_policy
         episode.episode = []
-        if episode.generate(Racetrack):
+        if episode.generate(racetrack):
             if (
                 incremental_prediction(
-                    Racetrack, episode.episode, cum_IS, epsilon, gamma
+                    racetrack, episode.episode, cum_is, epsilon, gamma
                 )
-                == False
+                == false
             ):
-                behaviour_policy = utils.get_policy(Racetrack, epsilon)
+                behaviour_policy = utils.get_policy(racetrack, epsilon)
                 continue
             episode_count += 1  # successful attempt counter
             ep_generation_attempt += 1  # general attempt counter
@@ -91,46 +110,42 @@ def off_policy_control(
                 episode_count >= max_episode_count
             ):
                 print(
-                    "Off-policy control achieved minimum successful episodes for each starting location, ending run..."
+                    "off-policy control achieved minimum successful episodes for each starting location, ending run..."
                 )
                 break
             print(
-                f"Off-policy control successful at episode {episode_count}, continuing..."
+                f"off-policy control successful at episode {episode_count}, continuing..."
             )
         else:
             ep_generation_attempt += 1  # general attempt counter
             print(
-                f"Episode generation reached max step, generation attempt {ep_generation_attempt} failed..."
+                f"episode generation reached max step, generation attempt {ep_generation_attempt} failed..."
             )
             if ep_generation_attempt > max_episode_generation_attempt:
                 if epsilon < 1:
                     epsilon += 0.01
                     print(
-                        f"Retrying with new behaviour policy with epsilon value of {round(epsilon, 2)} ...."
+                        f"retrying with new behaviour policy with epsilon value of {round(epsilon, 2)} ...."
                     )
                 else:
                     print(
-                        "Epsilon value at maximum of 0.99, stopping off-policy control."
+                        "epsilon value at maximum of 0.99, stopping off-policy control."
                     )
                     break
                 ep_generation_attempt = 0
-            behaviour_policy = utils.get_policy(Racetrack, epsilon)
+            behaviour_policy = utils.get_policy(racetrack, epsilon)
 
-    print("Off-policy completed.")
+    print("off-policy completed.")
 
 
 def main():
-    race_track = ["#######E", 
-                  "#NNNNNNE", 
-                  "#NNNNNNE", 
-                  "#NNNNNNE", 
-                  "#SS#####"]
+    race_track = ["#######e", "#nnnnnne", "#nnnnnne", "#nnnnnne", "#ss#####"]
 
     epsilon = 0.1
     gamma = 0.9
     max_episode_count = 1000
     max_episode_generation_attempt = 4
-    race_track_obj = utils.Racetrack(race_track)
+    race_track_obj = utils.racetrack(race_track)
 
     off_policy_control(
         race_track_obj,
