1ba715a5f8d7f0427efd09ab9b56dd56a497f9fc
justinpun728 <justinpun728@gmail.com>
2026-07-20 19:20:11 +0800

    added note to windy_exercise_1

--- temporal_difference/windy_gridworld/windy_exercise_1.py
+++ temporal_difference/windy_gridworld/windy_exercise_1.py
@@ -2,6 +2,8 @@ import gymnasium as gym
 import gymnasium_env
 from gymnasium.utils.env_checker import check_env
 
+# Note: gymnasium_env was imported as a local lib to be detected by script to check custom envs, else it would raise an Namespace error in the gymnasium lib
+
 env = gym.make("gymnasium_env/WindyGridWorld-v0")
 
 try:
